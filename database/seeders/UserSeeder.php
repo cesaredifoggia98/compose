93eca1afe61804e86113c124678408331dab9d2b
Cesare di Foggia <cesaredifoggia98>
2021-03-11 16:12:08 +0100

    primo commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')
+            ->where('email', 'difoggiac914@gmail.com')
+                ->update(['role' => 'admin']);
+    }
+}
